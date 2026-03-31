d26bc7afe0f263f889a46624b1df07a98fe33d1b
hitotonazo <arg.observerx@gmail.com>
2026-03-31 23:12:36 +0900

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.NFE_CONFIG = {
   // Cloudflare R2 の公開URLに差し替えてください
   // 例: https://pub-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.r2.dev/next-field-education
-  R2_PUBLIC_BASE: "https://pub-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.r2.dev/next-field-education",
+  R2_PUBLIC_BASE: "https://pub-4952ba4a72ee4fb5923495a3626649d3.r2.dev/images",
   SHARE_URL_STORAGE_KEY: "nextfield_share_url"
 };
